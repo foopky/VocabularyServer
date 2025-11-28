60aa21c175f9674a46c60ade17d74716d8660c60
ckhsa03 <ckhsa03@gmail.com>
2025-11-28 13:50:36 +0900

    fix: cors

--- src/main/java/vocabulary/app/config/WebConfig.java
+++ src/main/java/vocabulary/app/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("https://vocawebvercel.vercel.app")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
